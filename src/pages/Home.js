44b5ef85144f9ef4a791ed94f41d6bdb0c96eb65
Lissette <lgorrinr@my.centennialcollege.ca>
2024-08-08 15:34:54 -0400

    frontend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Home.css';
 import robotChefLogo from '../image/Robot-Chef.png';
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <div className="home-container">
       <div className="home-content">
@@ -21,7 +23,7 @@ function Home() {
           </div>
         </div>
       </div>
-    <div className="home">
+    <div className="home"></div>
       <div className="hero">
         <h1>Lissette Gorrin</h1>
         <p>Expert Digital Marketer specializing in Social Media and Video Production.</p>

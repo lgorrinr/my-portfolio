6c0171e9dd02c9dc6ef25dc8e0a6511e8e736931
Lissette <lgorrinr@my.centennialcollege.ca>
2024-08-08 15:28:02 -0400

    Frontend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
+import robotChefLogo from '../image/Robot-Chef.png';
 
 function Home() {
   return (
+    <div className="home-container">
+      <div className="home-content">
+        <div className="home-logo">
+          <img src={robotChefLogo} alt="Robot Chef Logo" />
+        </div>
+        <div className="home-text">
+          <h1><b>Lissette Gorrin</b></h1>
+          <p>
+          Expert Digital Marketer specializing in Social Media and Video Production.
+          </p>
+          <div className="home-buttons">
+            <button onClick={() => navigate('/register')} className="btn">Register</button>
+            <button onClick={() => navigate('/login')} className="btn">Log In</button>
+          </div>
+        </div>
+      </div>
     <div className="home">
       <div className="hero">
         <h1>Lissette Gorrin</h1>
